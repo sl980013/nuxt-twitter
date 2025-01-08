4f14490f5ed147e6e2fe8952ce86123a11ee047a
Seoeun Lee <anytimelse98@gmail.com>
2025-01-08 01:20:42 +0000

    initial commit

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  <div class="{'dark': darkMode}">
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-red-500 dark:text-dim-600">Twitter clone</h1>
   </div>
 </template>
+<script setup>
+const darkMode = ref(true)
+</script>
